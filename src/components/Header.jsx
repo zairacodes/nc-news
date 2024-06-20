667d466699b028f933dc5552e7796a3516cdeb91
zairacodes <zaira.nuovo@gmail.com>
2024-06-20 14:28:44 +0100

    update with user login/logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,67 @@
 import { Link } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { getUsers } from "../utils/api";
 
 const Header = () => {
+  const [users, setUsers] = useState([]);
+  const { user, setUser } = useContext(UserContext);
+  const [err, setErr] = useState("");
+
+  useEffect(() => {
+    getUsers()
+      .then((usersFromApi) => {
+        setUsers(usersFromApi);
+      })
+      .catch((err) => {
+        setErr(err);
+      });
+  }, []);
+
+  const handleSelect = (e) => {
+    if (e.target.value === "logout") {
+      setUser("");
+    } else {
+      const selectedUser = users.find((u) => u.username === e.target.value);
+      setUser(selectedUser);
+    }
+  };
+
+  if (err)
+    return (
+      <p className="err-msg">
+        Oops! Something went wrong, please try again later.
+      </p>
+    );
+
   return (
     <header>
       <Link to="/">
         <h1>NC News</h1>
       </Link>
+      <div className="login-dropdown">
+        <label>Log in: </label>
+        <select
+          onChange={handleSelect}
+          value={user.username || ""}
+          className="usernames-dropdown"
+        >
+          <option value="" disabled>
+            Select a user
+          </option>
+          {users.map((user) => (
+            <option key={user.username} value={user.username}>
+              {user.username}
+            </option>
+          ))}
+          <option value="logout">Log out</option>
+        </select>
+      </div>
+      {!user.username ? (
+        <p>Not logged in</p>
+      ) : (
+        <p>Logged in as: {user.username}</p>
+      )}
     </header>
   );
 };
